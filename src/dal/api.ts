381bccf059a631e0f61246c2b956390b7b54463b
nlklfor <nik.slepcov1771@gmail.com>
2025-12-24 15:15:58 +0100

    created cond w/ apiKey

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -1,32 +1,39 @@
-import type { TrackType } from "@/interfaces/tracks"
+import type { TrackType } from "@/interfaces/tracks";
 
 interface getTracksType {
-    data: TrackType[]
+  data: TrackType[];
 }
 
 interface getTrackLyricsType {
-    data: TrackType
+  data: TrackType;
 }
+
+const prepareHeaders = () => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  if (!apiKey) return undefined;
+  return {
+    "api-key": apiKey,
+  };
+};
 
 export const getTracks = () => {
-    const promise: Promise<getTracksType> =
-        fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-            headers: {
-                "api-key": import.meta.env.VITE_API_KEY ,
-            }
-        }).then((response) => response.json())
+  const promise: Promise<getTracksType> = fetch(
+    "https://musicfun.it-incubator.app/api/1.0/playlists/tracks",
+    {
+      headers: prepareHeaders(),
+    }
+  ).then((response) => response.json());
 
-    return promise;
-}
-
+  return promise;
+};
 
 export const getTrackLyrics = (selectedTrackId: string) => {
-    const promise: Promise<getTrackLyricsType> =
-        fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${selectedTrackId}`, {
-            headers: {
-                "api-key": import.meta.env.VITE_API_KEY,
-            }
-        }).then((response) => response.json())
+  const promise: Promise<getTrackLyricsType> = fetch(
+    `https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${selectedTrackId}`,
+    {
+      headers: prepareHeaders(),
+    }
+  ).then((response) => response.json());
 
-    return promise
-}
+  return promise;
+};
